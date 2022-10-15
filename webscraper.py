acac0e1622861ac063976838d8500833269d2b49
drmtbishop <drmtbishop@gmail.com>
2022-10-15 20:16:30 +0100

    add SWA

--- webscraper.py
+++ webscraper.py
@@ -29,13 +29,17 @@ searchterm = input("Search term: ").replace(" ","+");
 # Whisky Hammer search
 whdata = {}
 def wh():
+	whdata = {}
 	wh_url = "https://www.whiskyhammer.com/auction/past/q-"+searchterm+"/?sortby=end-time&ps=1000"
 	#wh_url = "https://www.whiskyhammer.com/auction/past/q-"+searchterm+"/"
 	wh_htmlcode = requests.get(wh_url).content
 	wh_data = BeautifulSoup(wh_htmlcode, 'html.parser')
 	wh_auctionlist = wh_data.find('div', {'id':'browse'})
 	wh_bottlelist = re.search("\[\{.+\}\]", str(wh_auctionlist))
-	wh_bottlelisttrim = wh_bottlelist.group()[1:-1]
+	try:
+		wh_bottlelisttrim = wh_bottlelist.group()[1:-1]
+	except AttributeError:
+		return (print ('Whisky Hammer: No bottle found'))
 
 	whiskyHammer={}
 	for item in wh_bottlelisttrim.split("}"):
@@ -64,15 +68,26 @@ def wh():
 # Whisky Auctioneer search
 wadata = {}
 def wa():
+	wasearchterm = str(searchterm.replace('+', '%20'))
 	# Getting page by page data NOTE: page=1 is the second page
 	# wa_url_page = "https://whiskyauctioneer.com/auction-search?items_per_page=500&sort=field_reference_field_end_date%20DESC&text=daftmill&page=1"
-	wa_url_page = "https://whiskyauctioneer.com/auction-search?text="+searchterm+"&sort=field_reference_field_end_date+DESC&items_per_page=500&f%5B0%5D=cask_type%3A41"
-	wa_htmlcode = requests.get(wa_url_page).content
+	#wa_url_page = "https://whiskyauctioneer.com/auction-search?text="+wasearchterm+"&sort=field_reference_field_end_date+DESC&items_per_page=500&f%5B0%5D=cask_type%3A41"
+	#wa_url_page = "https://whiskyauctioneer.com/auction-search?text="+searchterm+"&sort=field_reference_field_end_date%20DESC"
+	headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',}
+	wa_url_page = "https://whiskyauctioneer.com/auction-search?text="+wasearchterm
+	wa_htmlcode = requests.get(wa_url_page, headers = headers).content
+	#print (type(wa_htmlcode))
 	wa_data = BeautifulSoup(wa_htmlcode, 'html.parser')
+	#print (type(wa_data))
+
+	#print (wa_data.find('div',{'class':'item-list'}))
+	#.find('a', {'title':'Go to last page'})['href'])
 	try:
-		wa_lastpage = int(wa_data.find('li', {'class':'pager-last last'}).find('a').get('href')[-1])
+		#wa_lastpage = int(wa_data.find('li', {'class':'pager-last last'}).find('a').get('href')[-1])
+		wa_lastpage = int(wa_data.find('li', {'class':'pager-last last'}).find('a').get('href').split('page=')[1])
 	except (IndexError, AttributeError):
 		wa_lastpage = 0
+	#print ('lastpage variable is: ',wa_lastpage)
 	print ('Getting Whisky Auctioneer data from '+str(wa_lastpage+1)+' total page(s)...')
 	# Loop through pages
 	whiskyAuctioneer={}
@@ -80,10 +95,12 @@ def wa():
 	for eachpage in range(wa_lastpage+1):
 		print (eachpage)
 		#wa_url = "https://whiskyauctioneer.com/auction-search?items_per_page=500&sort=field_reference_field_end_date%20DESC&text="+searchterm+"&page="+str(eachpage)
-		wa_url = "https://whiskyauctioneer.com/auction-search?text="+searchterm+"&sort=field_reference_field_end_date+DESC&items_per_page=500&f%5B0%5D=cask_type%3A41&page="+str(eachpage)
+		#wa_url = "https://whiskyauctioneer.com/auction-search?text="+searchterm+"&sort=field_reference_field_end_date+DESC&items_per_page=500&f%5B0%5D=cask_type%3A41&page="+str(eachpage)
+		wa_url = "https://whiskyauctioneer.com/auction-search?text="+wasearchterm+"&sort=field_reference_field_end_date+DESC&page="+str(eachpage)
+		#wa_url = "https://whiskyauctioneer.com/auction-search?text="+searchterm+"&sort=field_reference_field_end_date+DESC&items_per_page=500&f%5B0%5D=cask_type%3A41&page="+str(eachpage)
 		#print (wa_url)
 		#wa_url = "https://whiskyauctioneer.com/auction-search?text="+searchterm+"&sort=field_reference_field_end_date+DESC&items_per_page=500&f%5B0%5D=cask_type%3A41"
-		wa_htmlcode = requests.get(wa_url).content
+		wa_htmlcode = requests.get(wa_url, headers = headers).content
 		wa_data = BeautifulSoup(wa_htmlcode, 'html.parser')
 		wa_auctionlist = wa_data.find('div', {'class':'view-content'})
 		try:
@@ -119,7 +136,7 @@ def wa():
 				continue
 			#break
 	#print (len(whiskyAuctioneer))
-
+	print ("\n"+"Scotch Whisky Auctions:")
 	wadata = {};
 	for bottle in whiskyAuctioneer:
 		try:
@@ -129,6 +146,7 @@ def wa():
 
 	#return results_plot(wadata, 'Whisky Auctioneer')
 	print ("WA Records: "+str(len(wadata)))
+	#print (wadata)
 	return wadata
 
 
@@ -247,6 +265,85 @@ def gw():
 	print ("GW Records: "+str(len(gwdata)))
 	return gwdata
 
+# Scotch Whisky Auctions search
+swadata = {}
+swaAuctionDict={
+'201' : '18-NOV-2017', '045' : '01-JAN-2015', '046' : '01-FEB-2015', '047' : '01-MAR-2015', '048' : '01-APR-2015', 
+'049' : '01-MAY-2015', '050' : '01-JUN-2015', '051' : '01-JUL-2015', '052' : '02-AUG-2015', '053' : '01-SEP-2015', 
+'054' : '01-OCT-2015', '055' : '01-NOV-2015', '056' : '06-DEC-2015', '057' : '01-JAN-2016', '058' : '07-FEB-2016', 
+'059' : '01-MAR-2016', '060' : '01-APR-2016', '061' : '01-MAY-2016', 
+'062' : '05-JUN-2016','063' : '06-JUL-2016', '064' : '07-AUG-2016', '065' : '04-SEP-2016', '066' : '02-OCT-2016',
+'067' : '06-NOV-2016', '068' : '04-DEC-2016', '069' : '03-JAN-2017', '070' : '05-FEB-2017', '071' : '05-MAR-2017', '072' : '02-APR-2017', 
+'073' : '07-MAY-2017', '074' : '04-JUN-2017', '075' : '02-JUL-2017', '076' : '06-AUG-2017', '077' : '03-SEP-2017', '078' : '01-OCT-2017', 
+'079' : '05-NOV-2017', '080' : '03-DEC-2017', '081' : '07-JAN-2018', '082' : '04-FEB-2018', '083' : '04-MAR-2018', '084' : '01-APR-2018', 
+'085' : '06-MAY-2018', '086' : '03-JUN-2018', '087' : '01-JUL-2018', '088' : '05-AUG-2018', '089' : '02-SEP-2018', '090' : '07-OCT-2018', 
+'091' : '04-NOV-2018', '092' : '02-DEC-2018', '093' : '06-JAN-2019', '094' : '03-FEB-2019', '095' : '03-MAR-2019', '096' : '07-APR-2019', 
+'097' : '05-MAY-2019', '098' : '02-JUN-2019', '099' : '07-JUL-2019', '100' : '04-AUG-2019', '101' : '01-SEP-2019', '102' : '06-OCT-2019', 
+'103' : '03-NOV-2019', '104' : '01-DEC-2019', '105' : '05-JAN-2020', '106' : '02-FEB-2020', '107' : '01-MAR-2020', '108' : '07-JUN-2020', 
+'109' : '05-JUL-2020', '110' : '02-AUG-2020', '111' : '06-SEP-2020', '112' : '04-OCT-2020', '113' : '01-NOV-2020', '114' : '06-DEC-2020', 
+'115' : '05-JAN-2021', '116' : '07-FEB-2021', '117' : '07-MAR-2021', '118' : '04-APR-2021', '119' : '02-MAY-2021', '120' : '06-JUN-2021', 
+'121' : '04-JUL-2021', '122' : '01-AUG-2021', '123' : '05-SEP-2021', '124' : '03-OCT-2021', '125' : '07-NOV-2021', '126' : '05-DEC-2021', 
+'127' : '09-JAN-2022', '128' : '13-FEB-2022', '129' : '13-MAR-2022', '130' : '10-APR-2022', '131' : '08-MAY-2022', '132' : '12-JUN-2022', 
+'133' : '10-JUL-2022', '134' : '14-AUG-2022', '135' : '11-SEP-2022', '136' : '09-OCT-2022'}
+def swa():
+	# Find total number of pages
+	swa_url_page = "https://www.scotchwhiskyauctions.com/auctions/all/?q="+searchterm+"&search=a"
+	swa_htmlcode = requests.get(swa_url_page).content
+	swa_data = BeautifulSoup(swa_htmlcode, 'html.parser')
+	swa_pagelist = swa_data.find('div', {'id':'lotswrap'}).find('h3').text
+	swa_lastpage = int(int(swa_pagelist.split(' ',2)[1])/20)+1
+
+#	try:
+#		swa_lastpage = int(swa_pagelist[-2].contents[0])
+#	except IndexError:
+#		swa_lastpage = 1
+#
+	print ('Getting Scotch Whisky Auctions data from '+str(swa_lastpage)+' total page(s)...')
+
+	scotchWhiskyAuctions={}
+	tempdict={'lot':'','title':'','price':'','date':''}
+	for eachpage in range(swa_lastpage):
+		print (eachpage+1)
+		swa_url = "https://www.scotchwhiskyauctions.com/auctions/all/?q="+searchterm+"&search=a&page="+str(eachpage+1)
+		swa_htmlcode = requests.get(swa_url).content
+		swa_data = BeautifulSoup(swa_htmlcode, 'html.parser')
+		swa_auctionlist = swa_data.find('div', {'id':'lots'}).find_all('a')
+		#print (swa_auctionlist)
+		for entry in swa_auctionlist:
+			tempdict = {}
+			tempdict['title']=entry.find('h4').text
+			#print (entry.find('a').find('h4').text)
+			try:
+				tempdict['price']=float(entry.find('p', {'class':'sold'}).get_text().split(u"\xA3",1)[1].replace("," , ""))
+			except AttributeError:
+				break
+			#print (float(entry.find('p', {'class':'sold'}).get_text().split(u"\xA3",1)[1].replace("," , "")))
+
+			tempdict['lot']=entry.find('h6').text.split(' ',2)[2]
+			#print (entry.find('a').find('h6').text.split(' ',2)[2])
+
+			tempkey = tempdict['lot']
+			#Date - comes from the lot id eg 127-01423 is the 127th auction so need a lookup dict of number:date
+			auctionNumber = str(tempdict['lot'][0:3])
+			tempdict['date'] = swaAuctionDict[auctionNumber]
+			#print (tempdict['date'])
+			newdict = {tempkey : tempdict}
+			scotchWhiskyAuctions.update(newdict)
+		else:
+			continue
+		break
+	print ("\n"+"Scotch Whisky Auctions:")
+	swadata = {};
+	for bottle in scotchWhiskyAuctions:
+		swadata.update({scotchWhiskyAuctions[bottle]['lot'] : {str(scotchWhiskyAuctions[bottle]['date']) : scotchWhiskyAuctions[bottle]['price']}})
+	#return results_plot(swadata, 'Scotch Whisky Auctions')
+	print ("SWA Records: "+str(len(swadata)))
+	#print (swadata)
+	return swadata
+
+
+
+
 
 def results_plot(bottlelist, auction):
 #	Plot data
@@ -273,7 +370,7 @@ def results_plot(bottlelist, auction):
 	plt.show()
 
 
-def multiplot(wadata, whdata, jwdata, gwdata):
+def multiplot(wadata, whdata, jwdata, gwdata, swadata):
 	# Plot all lines on one plot
 	# WA data
 	waplotstuff = []
@@ -282,6 +379,7 @@ def multiplot(wadata, whdata, jwdata, gwdata):
 			waplotstuff.append((pltdates.datestr2num(key) , float(value)))
 	wadates = [x[0] for x in waplotstuff]
 	wavalues = [x[1] for x in waplotstuff]
+
 	# WH data
 	whplotstuff = []
 	for k,v in whdata.items():
@@ -289,6 +387,7 @@ def multiplot(wadata, whdata, jwdata, gwdata):
 			whplotstuff.append((pltdates.datestr2num(key) , float(value)))
 	whdates = [x[0] for x in whplotstuff]
 	whvalues = [x[1] for x in whplotstuff]
+
 	# JW data
 	jwplotstuff = []
 	for k,v in jwdata.items():
@@ -303,9 +402,16 @@ def multiplot(wadata, whdata, jwdata, gwdata):
 			gwplotstuff.append((pltdates.datestr2num(key) , float(value)))
 	gwdates = [x[0] for x in gwplotstuff]
 	gwvalues = [x[1] for x in gwplotstuff]
+	# SWA data
+	swaplotstuff = []
+	for k,v in swadata.items():
+		for key,value in v.items():
+			swaplotstuff.append((pltdates.datestr2num(key) , float(value)))
+	swadates = [x[0] for x in swaplotstuff]
+	swavalues = [x[1] for x in swaplotstuff]
 
 	# All values for maths
-	allValues = wavalues + whvalues + jwvalues + gwvalues
+	allValues = wavalues + whvalues + jwvalues + gwvalues + swavalues
 	meanValue = str("{:.2f}".format(float(stat.mean(allValues))))
 	maxValue = str(max(allValues))
 	minValue = str(min(allValues))
@@ -316,11 +422,13 @@ def multiplot(wadata, whdata, jwdata, gwdata):
 	whLabel = "Whisky Hammer (n="+str(len(whvalues))+")"
 	jwLabel = "Just Whisky (n="+str(len(jwvalues))+")"
 	gwLabel = "Grand Whisky Auction (n="+str(len(gwvalues))+")"
+	swaLabel = "Scotch Whisky Auctions (n="+str(len(swavalues))+")"
 	plt.figure(figsize=(10,5))
-	plt.plot(wadates, wavalues, marker = 'x', color = 'b', label = waLabel)
-	plt.plot(whdates, whvalues, marker = '*', color = 'r', label = whLabel)
-	plt.plot(jwdates, jwvalues, marker = 'o', color = 'c', label = jwLabel)
-	plt.plot(gwdates, gwvalues, marker = '+', color = 'g', label = gwLabel)
+	plt.scatter(wadates, wavalues, marker = 'x', color = 'b', label = waLabel)
+	plt.scatter(whdates, whvalues, marker = '*', color = 'r', label = whLabel)
+	plt.scatter(jwdates, jwvalues, marker = 'o', color = 'c', label = jwLabel)
+	plt.scatter(gwdates, gwvalues, marker = '+', color = 'g', label = gwLabel)
+	plt.scatter(swadates, swavalues, marker = 'v', color = 'y', label = swaLabel)
 	ax = plt.gca()
 	datemajor = pltdates.DateFormatter('%Y')
 	dateminor = pltdates.DateFormatter('%m:%Y')
@@ -342,5 +450,7 @@ def multiplot(wadata, whdata, jwdata, gwdata):
 #wa()
 #jw()
 #gw()
-multiplot(wa(), wh(), jw(), gw())
+#swa()
+
+multiplot(wa(), wh(), jw(), gw(), swa())
 
